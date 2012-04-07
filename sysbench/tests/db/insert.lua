338ed5dadb101dc618a657e6abde199aeb9312dd
Philip Stoev <pstoev@nuodb.com>
2012-04-07 09:50:20 +0300

    fix sysbench/tests/db/insert.lua to be NuoDb-compatible

--- sysbench/tests/db/insert.lua
+++ sysbench/tests/db/insert.lua
@@ -15,16 +15,17 @@ function event(thread_id)
 
    table_name = "sbtest".. sb_rand_uniform(1, oltp_tables_count)
 
-   if (oltp_auto_inc) then
-      i = 0
-   else
-      i = sb_rand_uniq(1, oltp_table_size)
-   end
    k_val = sb_rand(1, oltp_table_size)
    c_val = sb_rand_str([[
 ###########-###########-###########-###########-###########-###########-###########-###########-###########-###########]])
    pad_val = sb_rand_str([[
 ###########-###########-###########-###########-###########]])
-   
-   rs = db_query("INSERT INTO " .. table_name ..  " (id, k, c, pad) VALUES " .. string.format("(%d, %d, '%s', '%s')",i, k_val, c_val, pad_val))
+
+   if (oltp_auto_inc) then
+      rs = db_query("INSERT INTO " .. table_name ..  " (k, c, pad) VALUES " .. string.format("(%d, '%s', '%s')", k_val, c_val, pad_val))
+   else
+      i = sb_rand_uniq(1, oltp_table_size)
+      rs = db_query("INSERT INTO " .. table_name ..  " (id, k, c, pad) VALUES " .. string.format("(%d, %d, '%s', '%s')",i, k_val, c_val, pad_val))
+   end
+
 end
